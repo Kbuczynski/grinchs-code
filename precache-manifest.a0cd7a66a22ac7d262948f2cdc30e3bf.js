0089a2f77be8c65f4f7b8edc16c7b823eaf3fd96
Kbuczynski <44814273+Kbuczynski@users.noreply.github.com>
2019-11-02 21:46:59 +0100

    Updates

---
+++ precache-manifest.a0cd7a66a22ac7d262948f2cdc30e3bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329e40d285aefe1eb408f4fdc9ae5016",
+    "url": "/grinchs-code/index.html"
+  },
+  {
+    "revision": "4ab43e652ee62bc8253f",
+    "url": "/grinchs-code/static/css/main.786b675c.chunk.css"
+  },
+  {
+    "revision": "30564bf20369d3115e7e",
+    "url": "/grinchs-code/static/js/2.a6b4d57b.chunk.js"
+  },
+  {
+    "revision": "4ab43e652ee62bc8253f",
+    "url": "/grinchs-code/static/js/main.8909a825.chunk.js"
+  },
+  {
+    "revision": "fe21dc5e319680e9d1b2",
+    "url": "/grinchs-code/static/js/runtime-main.824c13a9.js"
+  },
+  {
+    "revision": "fdeb667532d8a394fe8b99282d230199",
+    "url": "/grinchs-code/static/media/avatar1.fdeb6675.png"
+  },
+  {
+    "revision": "837a944fa594fc1890fe553b8efe74ca",
+    "url": "/grinchs-code/static/media/avatar2.837a944f.png"
+  },
+  {
+    "revision": "ec9aab28128e6417c931848623eaa0b5",
+    "url": "/grinchs-code/static/media/avatar3.ec9aab28.png"
+  },
+  {
+    "revision": "5e1353d53b02adf31fb2e628d1478f59",
+    "url": "/grinchs-code/static/media/avatar4.5e1353d5.png"
+  },
+  {
+    "revision": "af4be400d737809c3fbea61753f8dd07",
+    "url": "/grinchs-code/static/media/home.af4be400.jpg"
+  },
+  {
+    "revision": "3037cf57e8b0eb5b3529801c6c548792",
+    "url": "/grinchs-code/static/media/logo-header.3037cf57.png"
+  }
+]);
